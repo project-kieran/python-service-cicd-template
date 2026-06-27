022635d2c5020d74c91a213562b2f5de1e07ba3c
project-Kieran <mcdonough_kieran@yahoo.co.uk>
2026-06-27 19:15:45 +0100

    Added linting and formatting

--- app/main.py
+++ app/main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI
 from datetime import datetime, timezone
 
+from fastapi import FastAPI
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my little service"}
 
+
 @app.get("/health")
 async def health():
     return {"status": "ok"}
 
+
 @app.get("/version")
 async def version():
     return {"version": "1.0.0"}
+
 
 @app.get("/time")
 async def time():
